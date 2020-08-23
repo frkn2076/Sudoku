1c36d92aa24e15a4e27cd9fa780571d6b43b5b65
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-08-23 13:58:35 +0300

    background added, buttons and draw algorithm set

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<List<int>> square00 = Sudoku.getInnerSquare(0, 0);
+  final List<List<int>> square01 = Sudoku.getInnerSquare(0, 1);
+  final List<List<int>> square02 = Sudoku.getInnerSquare(0, 2);
+  final List<List<int>> square10 = Sudoku.getInnerSquare(1, 0);
+  final List<List<int>> square11 = Sudoku.getInnerSquare(1, 1);
+  final List<List<int>> square12 = Sudoku.getInnerSquare(1, 2);
+  final List<List<int>> square20 = Sudoku.getInnerSquare(2, 0);
+  final List<List<int>> square21 = Sudoku.getInnerSquare(2, 1);
+  final List<List<int>> square22 = Sudoku.getInnerSquare(2, 2);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Sudoku'),
+      home: MyHomePage(title: 'Sudoku', squares: [
+        square00,
+        square01,
+        square02,
+        square10,
+        square11,
+        square12,
+        square20,
+        square21,
+        square22
+      ]),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title, this.squares}) : super(key: key);
 
   final String title;
+  final List<List<List<int>>> squares;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -30,124 +51,133 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     double widthOfDevice = MediaQuery.of(context).size.width;
-    var maskedSudoku = getSample();
 
     return Scaffold(
-        resizeToAvoidBottomInset: false, //Added for bottom overflow warning.
-        appBar: AppBar(
-          title: Text(widget.title),
+      resizeToAvoidBottomInset: false, //Added for bottom overflow warning.
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.fill,
+          ),
         ),
-        body: Container(
+        child: Container(
           padding: const EdgeInsets.all(9),
           child: Column(
             children: <Widget>[
               Row(
                 children: <Widget>[
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(3,6)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(6,9))
+                  createTable(widthOfDevice / 9, widget.squares[0]),
+                  createTable(widthOfDevice / 9, widget.squares[1]),
+                  createTable(widthOfDevice / 9, widget.squares[2])
                 ],
               ),
               Row(
                 children: <Widget>[
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3))
+                  createTable(widthOfDevice / 9, widget.squares[3]),
+                  createTable(widthOfDevice / 9, widget.squares[4]),
+                  createTable(widthOfDevice / 9, widget.squares[5])
                 ],
               ),
               Row(
                 children: <Widget>[
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3)),
-                  createTable(widthOfDevice / 9, maskedSudoku.sublist(0,3))
+                  createTable(widthOfDevice / 9, widget.squares[6]),
+                  createTable(widthOfDevice / 9, widget.squares[7]),
+                  createTable(widthOfDevice / 9, widget.squares[8])
                 ],
               ),
               SizedBox(
                 height: 70,
               ),
-              Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.grey)),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.done_outline,
-                    color: Colors.green,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black54),
+                        color: Colors.grey[200]),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.refresh,
+                        color: Colors.green,
+                      ),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
                   ),
-                  iconSize: 50,
-                  onPressed: () {},
-                ),
-              )
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black54),
+                        color: Colors.grey[200]),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.done_outline,
+                        color: Colors.green,
+                      ),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   Widget createTable(double cellHeight, List<List<int>> list) {
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-        ),
+            border: Border.all(color: Colors.black), color: Colors.white),
         child: Table(
           border: TableBorder.symmetric(
               inside: BorderSide(color: Colors.black),
               outside: BorderSide(color: Colors.black)),
           defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-          children: list.map((value) =>
-              TableRow(
-                    children: List.generate(3, (index) =>
-                       InkResponse(
+          children: list
+              .map((value) => TableRow(
+                    children: List.generate(
+                      3,
+                      (index) => InkResponse(
                         child: SizedBox(
                           height: cellHeight,
-                          child: 
-                          Text(value[index].toString())
-                          // TextField(
-                          //   readOnly: false,
-                          //   inputFormatters: <TextInputFormatter>[
-                          //     FilteringTextInputFormatter.digitsOnly,
-                          //     LengthLimitingTextInputFormatter(1)
-                          //   ],
-                          //   style: TextStyle(
-                          //     fontSize: 20,
-                          //   ),
-                          //   textAlign: TextAlign.center,
-                          //   decoration: InputDecoration(
-                          //       contentPadding:
-                          //           EdgeInsets.only(bottom: cellHeight / 4)),
-                          // ),
+                          child:
+                              // Text(value[index].toString())
+                              TextField(
+                            readOnly: value[index] != 0,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(1)
+                            ],
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                                hintText: value[index] != 0
+                                    ? value[index].toString()
+                                    : "",
+                                hintStyle: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                                contentPadding:
+                                    EdgeInsets.only(bottom: cellHeight / 4)),
+                          ),
                         ),
                       ),
                     ),
-                  )).toList(),
+                  ))
+              .toList(),
         ),
       ),
     );
-  }
-
-
-  List<List<int>> maskSudoku(){
-    Random random = Random();
-    var sudoku = getSample();
-    for (var i = 0; i < 7; i++) {
-      for (var i = 0; i < 7; i++) {
-         sudoku[random.nextInt(9)][random.nextInt(9)] = 0;
-      }
-    }
-    return sudoku;
-  }
-
-  List<List<int>> getSample(){
-    return [
-      [1,2,3,6,7,8,9,4,5],
-      [5,8,4,2,3,9,7,6,1],
-      [9,6,7,1,4,5,3,2,8],
-      [3,7,2,4,6,1,5,8,9],
-      [6,9,1,5,8,3,2,7,4],
-      [4,5,8,7,9,2,6,1,3],
-      [8,3,6,9,2,4,1,5,7],
-      [2,1,9,8,5,7,4,3,6],
-      [7,4,5,3,1,6,8,9,2]
-    ];
   }
 
   // List<List<int>> createSudoku() {
@@ -169,7 +199,7 @@ class _MyHomePageState extends State<MyHomePage> {
   //               for (var k = 0; k < 9; k++) {
   //                 if (sudoku[k][random2] == numberToAdd) {
   //                   break OUTER;
-  //                 } 
+  //                 }
   //               }
   //               sudoku[random1][random2] = numberToAdd;
   //               break OUTER;
@@ -194,5 +224,44 @@ class _MyHomePageState extends State<MyHomePage> {
   //   return availableOnes;
   // }
 
+}
 
+class Sudoku {
+  static List<List<int>> getInnerSquare(int row, int column) {
+    var sudoku = maskSudoku();
+    var innerSquare = List<List<int>>();
+    for (var i = row * 3; i < row * 3 + 3; i++) {
+      List<int> temp = List<int>();
+      for (var j = column * 3; j < column * 3 + 3; j++) {
+        temp.add(sudoku[i][j]);
+      }
+      innerSquare.add(temp);
+    }
+    return innerSquare;
+  }
+
+  static List<List<int>> maskSudoku() {
+    Random random = Random();
+    var sudoku = getSample();
+    for (var i = 0; i < 7; i++) {
+      for (var i = 0; i < 7; i++) {
+        sudoku[random.nextInt(9)][random.nextInt(9)] = 0;
+      }
+    }
+    return sudoku;
+  }
+
+  static List<List<int>> getSample() {
+    return [
+      [1, 2, 3, 6, 7, 8, 9, 4, 5],
+      [5, 8, 4, 2, 3, 9, 7, 6, 1],
+      [9, 6, 7, 1, 4, 5, 3, 2, 8],
+      [3, 7, 2, 4, 6, 1, 5, 8, 9],
+      [6, 9, 1, 5, 8, 3, 2, 7, 4],
+      [4, 5, 8, 7, 9, 2, 6, 1, 3],
+      [8, 3, 6, 9, 2, 4, 1, 5, 7],
+      [2, 1, 9, 8, 5, 7, 4, 3, 6],
+      [7, 4, 5, 3, 1, 6, 8, 9, 2]
+    ];
+  }
 }
